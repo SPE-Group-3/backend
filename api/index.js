d7b4051929dc678f75883f8c376f2701af5921b4
Anthony agyemang <agyemangduah520@gmail.com>
2025-03-05 16:51:59 +0000

    setting up JWT for user verification

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ import authRoute from "./routes/auth.js"
 import usersRoute from "./routes/users.js"
 import hotelRoute from "./routes/hotels.js"
 import roomsRoute from "./routes/rooms.js"
+import cookieParser from "cookie-parser";
+
 const app = express()
 dotenv.config()
 
@@ -26,7 +28,8 @@ mongoose.connection.on("connected", ()=>{
 })
 
 //middlewares
-app.use(express.json())
+app.use(cookieParser());
+app.use(express.json());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
